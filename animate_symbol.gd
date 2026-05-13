a5a0ec72209d62a6107f6a16a6a77b62325e2041
cherrythecool <138817584+cherrythecool@users.noreply.github.com>
2026-05-13 08:47:07 -0400

    Remove compatibility for `AnimateSymbol` as a class name

--- animate_symbol.gd
+++
@@ -1,8 +0,0 @@
-@tool
-## @deprecated: Legacy class name, use [AnimateSymbol2D] which follows the newer naming conventions of Godot.
-##
-## Added for compatibility's sake, migrate to [AnimateSymbol2D] wherever
-## possible. (This is only name compatibility, everything else is different
-## and this exists to keep scenes loading for previous users of gdanimate)
-extends AnimateSymbol2D
-class_name AnimateSymbol
